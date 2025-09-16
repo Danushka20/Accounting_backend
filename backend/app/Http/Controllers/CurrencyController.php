b8d826ba834901a4083d7153db024fe39dca4a33
NalinPriyankara <nalinpriyankara033@gmail.com>
2025-09-16 07:50:27 +0530

    init

--- backend/app/Http/Controllers/CurrencyController.php
+++ backend/app/Http/Controllers/CurrencyController.php
@@ -36,13 +36,6 @@ class CurrencyController extends Controller
             'currency_name' => 'required|string|max:255',
             'hundredths_name' => 'nullable|string|max:255',
             'country' => 'required|string|max:255',
-            'auto_exchange_rate_update' => 'boolean'
-        ]);
-
-        $currency = Currency::create($validatedData);
-
-        return response()->json($currency, 201);
-
             'auto_exchange_rate_update' => 'sometimes|boolean'
         ]);
 
@@ -50,12 +43,12 @@ class CurrencyController extends Controller
             "currency_abbreviation" => $validatedData['currency_abbreviation'],
             "currency_symbol" => $validatedData['currency_symbol'],
             "currency_name" => $validatedData['currency_name'],
-            "hundredths_name" => $validatedData['hundredths_name'],
+            "hundredths_name" => $validatedData['hundredths_name'] ?? null,
             "country" => $validatedData['country'],
             "auto_exchange_rate_update" => $validatedData['auto_exchange_rate_update'] ?? false,
         ]);
 
-        return response()->json($currency);
+        return response()->json($currency, 201);
     }
 
     /**
@@ -89,7 +82,7 @@ class CurrencyController extends Controller
             'currency_name' => 'required|string|max:255',
             'hundredths_name' => 'nullable|string|max:255',
             'country' => 'required|string|max:255',
-            'auto_exchange_rate_update' => 'boolean'
+            'auto_exchange_rate_update' => 'sometimes|boolean'
         ]);
 
         $currency->update($validatedData);
